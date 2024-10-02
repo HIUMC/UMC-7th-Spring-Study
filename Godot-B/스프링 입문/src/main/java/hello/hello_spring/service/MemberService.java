c2e5748a64fd1ea35d270bf79be09befa016ad38
SungWoo Bae <bright_aid@naver.com>
2024-10-02 18:17:10 +0900

    배성우 스프링 입문

---
+++ Godot-B/스프링 입문/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,64 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    // 서비스는 보통 비즈니스에 의존적으로 설계, 리포지토리는 약간 더 기계적인 로직과 단순 처리
+    // 네이밍에서도 뉘앙스 드러남.
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        // memberRepository.findByName(member.getName()); 에서 Ctrl + Alt + V
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        // 아래는 축약한 버전
+//        memberRepository.findByName(member.getName())
+//                .ifPresent(m -> {
+//                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+//                });
+
+        // 메소드 추출 : Ctrl + Alt + Shift + T
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
