59f28fc964e393bf697b11e8933061443c02a416
안혜연 <ahy020606@naver.com>
2024-10-06 03:59:04 +0900

    Feat: API @ResponseBody 사용

--- noeyeyh/1주차/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ noeyeyh/1주차/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package hello.hello_spring.controller;
 
+import io.micrometer.observation.transport.Propagator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +20,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloSpring(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name", required = false) String name, Model model) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
