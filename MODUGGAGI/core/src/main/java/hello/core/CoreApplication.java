7b71214ae641c4eb056bd3a364a185c4ac1757a1
MODUGGAGI <kiroro0814@naver.com>
2024-10-05 23:09:41 +0900

    pr

--- MODUGGAGI/core/src/main/java/hello/core/CoreApplication.java
+++ MODUGGAGI/core/src/main/java/hello/core/CoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreApplication {
 	//Spring 기본 정리 push!!!!!!
+	//pr 확인용
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
