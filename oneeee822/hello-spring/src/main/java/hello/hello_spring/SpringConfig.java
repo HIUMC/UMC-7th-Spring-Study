2952cc644a1164c48905303725491a773b9077f2
oneeee822 <bestoneeee@naver.com>
2024-10-05 15:38:50 +0900

    스프링입문

---
+++ oneeee822/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,17 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
