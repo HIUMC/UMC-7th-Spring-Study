c60a09ebac08705d4c764f20ae87bc443a2f63c5
이찬우(Admin) <ichan-uadmin@Rachels-MacBook-3.local>
2024-10-05 20:13:26 +0900

    1주차 제출

---
+++ chan0831/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+}
