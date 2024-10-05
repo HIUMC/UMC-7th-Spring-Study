3e80c0e1027bb54d0d0121121b2876cca66d57fe
MODUGGAGI <kiroro0814@naver.com>
2024-10-05 22:15:08 +0900

    MODUGGAGI push

---
+++ MODUGGAGI/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,30 @@
+package hello.hellospring.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //디비가 알아서 생성해주는 건 Identity
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
