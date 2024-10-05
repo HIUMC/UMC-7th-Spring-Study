98b6fa407433dee59dff61305b79eb6a9324f9da
MODUGGAGI <kiroro0814@naver.com>
2024-10-05 22:24:32 +0900

    스프링 기본

--- MODUGGAGI/core/src/main/java/hello/core/AppConfig.java
+++ MODUGGAGI/core/src/main/java/hello/core/AppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
     //@Bean memberService -> new MemoryMemberRepository()
     //@Bean orderService -> new MemoryMemberRepository()
 
--- MODUGGAGI/core/src/main/java/hello/core/CoreApplication.java
+++ MODUGGAGI/core/src/main/java/hello/core/CoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-
+	//Spring 기본 정리 push
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
