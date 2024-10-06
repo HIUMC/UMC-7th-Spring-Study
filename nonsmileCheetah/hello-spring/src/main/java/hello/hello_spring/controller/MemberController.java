b3fae52e56854a9f8c1d3f8cf1affb5b8f5fc310
박건우 <pgw31111@gmail.com>
2024-10-06 22:01:00 +0900

    박건우 스프링 입문

--- nonsmileCheetah/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ nonsmileCheetah/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.hello_spring.controller;
 
+import ch.qos.logback.core.model.Model;
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,4 +18,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String newMember() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
+
