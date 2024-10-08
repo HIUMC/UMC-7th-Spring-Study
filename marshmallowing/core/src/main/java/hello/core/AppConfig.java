ec1836663868123ed403d6193189ad2b72d0a580
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-10-08 18:15:19 +0900

    Feat. AppConfig 리팩터링 까지

--- marshmallowing/core/src/main/java/hello/core/AppConfig.java
+++ marshmallowing/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,27 @@
 package hello.core;
 
-public class AppConfig {
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
+    }
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
