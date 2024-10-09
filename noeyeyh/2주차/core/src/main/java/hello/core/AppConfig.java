fff4b8f9c289178cf14a4216dad6a2f20daa1d39
안혜연 <ahy020606@naver.com>
2024-10-10 02:12:21 +0900

    Feat: 새로운 구조와 할인 정책 적용

--- noeyeyh/2주차/core/src/main/java/hello/core/AppConfig.java
+++ noeyeyh/2주차/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -25,6 +26,6 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
