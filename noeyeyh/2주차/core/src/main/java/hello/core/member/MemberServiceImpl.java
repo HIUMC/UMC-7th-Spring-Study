b9ad3b427da140660db759e3ab4773beb2988caf
안혜연 <ahy020606@naver.com>
2024-10-10 01:57:39 +0900

    Feat: 관심사의 분리

--- noeyeyh/2주차/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ noeyeyh/2주차/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,15 +2,16 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-    @Override
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void join(Member member) {
         memberRepository.save(member);
     }
 
-    @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
