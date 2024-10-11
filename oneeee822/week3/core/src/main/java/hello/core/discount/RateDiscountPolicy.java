dc0bff19fdff104bb08e3fe4efea58ee5a23c315
OneE <bestoneeee@naver.com>
2024-10-12 00:55:49 +0900

    [희로애락/정원희] 스프링 핵심 섹션 7

--- oneeee822/week3/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ oneeee822/week3/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,7 +3,10 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
