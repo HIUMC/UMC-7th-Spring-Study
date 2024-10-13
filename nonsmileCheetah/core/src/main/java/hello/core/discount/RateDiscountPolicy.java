e1a3db05bf85409879c5dbcee85b644c3babe123
박건우 <pgw31111@gmail.com>
2024-10-14 00:08:59 +0900

    Feat 박건우(ínonsmileCheetah) 3주차 내용 추가

--- nonsmileCheetah/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ nonsmileCheetah/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class RateDisocuntPolicy implements DiscountPolicy {
+public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
 
