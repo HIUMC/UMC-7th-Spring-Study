fd2daf174a552763f9ba77c57cd97c6153a99aac
SungWoo Bae <bright_aid@naver.com>
2024-10-16 14:13:39 +0900

    스프링 핵심원리 기본편 - 2

--- Godot-B/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ Godot-B/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,8 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; //10% 할인
