77d097621a3985e033a835267c01e432368e44f0
challonsy <musain2019@naver.com>
2024-10-31 15:03:17 +0900

    4주차

--- challonsy/SpringCorePrinciples-basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ challonsy/SpringCorePrinciples-basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )  //@Component가 붙은 클래스를 찾아서 빈으로 등록
 public class AutoAppConfig {
