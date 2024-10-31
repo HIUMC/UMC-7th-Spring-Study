02db13f3a8cb0d51f4747be0431c89c248d7a9ba
안혜연 <ahy020606@naver.com>
2024-10-31 09:38:43 +0900

    Feat: 의존관계 자동 주입

--- noeyeyh/2주차/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ noeyeyh/2주차/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +15,7 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy
-            discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
