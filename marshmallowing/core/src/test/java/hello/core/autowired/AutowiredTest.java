e512bcb07439dd02600d690651100d5c501f6f13
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-11-01 13:24:04 +0900

    Feat. 옵션 처리

---
+++ marshmallowing/core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,34 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+        //스프링 빈으로 등록
+    }
+
+    static class TestBean{
+        //호출 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("setNoBean1 = " + noBean1);
+        }
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("setNoBean2 = " + noBean2);
+        }
+        //Optional.empty 호출
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("setNoBean3 = " + noBean3);
+        }
+    }
+}
