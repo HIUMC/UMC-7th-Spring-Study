f180d6c5783fc8b7cdf9e6408b37a5d7a1432853
MODUGGAGI <kiroro0814@naver.com>
2024-11-05 17:31:15 +0900

    jpa 활용 1주차

---
+++ MODUGGAGI/jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+}
