4966df4c959f3417242f10cd11dfccdc7e219f21
Yeohuiju <yeohuiju03@gmail.com>
2024-11-06 21:16:33 +0900

    workbook CH5

---
+++ Yeohuiju/spring/src/main/java/umc/spring/domain/Region.java
@@ -0,0 +1,27 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.MemberMission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<Store> StoreList = new ArrayList<>();
+}
---
+++ Yeohuiju/spring/src/main/java/umc/spring/domain/Store.java
@@ -0,0 +1,39 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.MemberMission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String address;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+}
