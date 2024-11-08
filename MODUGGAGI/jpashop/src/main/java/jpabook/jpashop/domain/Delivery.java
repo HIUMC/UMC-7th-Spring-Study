61a103efab1cc98a3e03b73664cbe786e3439e4f
MODUGGAGI <kiroro0814@naver.com>
2024-11-08 13:48:25 +0900

    엔티티 개발1 강의

---
+++ MODUGGAGI/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", cascade = CascadeType.ALL)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(value = EnumType.STRING)
+    private DeliveryStatus status; //[READY, COMP]
+}
---
+++ MODUGGAGI/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus status; //주문 상대 [ORDER, CANCEL]
+}
