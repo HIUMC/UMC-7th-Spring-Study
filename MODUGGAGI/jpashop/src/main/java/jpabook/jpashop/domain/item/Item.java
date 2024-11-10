50ae5f38ca74be311e01e10fb6b75e9e152f078f
MODUGGAGI <kiroro0814@naver.com>
2024-11-10 18:23:34 +0900

    6주차 완료

--- MODUGGAGI/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ MODUGGAGI/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock"); //핵심 비즈니스 로직은 엔티티가 가지고 있는게 좋다?
+        }
+        this.stockQuantity = restStock;
+    }
 }
