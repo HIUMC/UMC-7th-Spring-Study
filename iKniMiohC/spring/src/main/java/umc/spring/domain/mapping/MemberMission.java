7701d0eb2d7e104c03fcecf782f13e74194f5bba
iKniMiohC <fnwm7401@naver.com>
2024-11-11 14:58:13 +0900

    Spring 5주차 워크북 미션 완료

---
+++ iKniMiohC/spring/src/main/java/umc/spring/domain/mapping/MemberMission.java
@@ -0,0 +1,21 @@
+package umc.spring.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.MissionStatus;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
