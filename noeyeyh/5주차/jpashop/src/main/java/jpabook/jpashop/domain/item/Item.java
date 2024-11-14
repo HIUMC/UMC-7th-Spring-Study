b18fd6aa02bac7d2ce6b0d9af88933287d865631
안혜연 <ahy020606@naver.com>
2024-11-14 15:54:43 +0900

    Feat: 상품 엔티티 비즈니스 로직 추가

--- noeyeyh/5주차/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ noeyeyh/5주차/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "item")
     private List<Category> category = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
