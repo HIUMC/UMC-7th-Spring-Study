c91b3c1e389b9265932529461c9a43938635ca24
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-11-21 14:00:47 +0900

    Feat: 회원 목록 조회

--- marshmallowing/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ marshmallowing/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.controller;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 //import javax.validaion이 안됨
 
@@ -37,5 +40,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    //추가
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
