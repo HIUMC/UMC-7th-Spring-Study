07f69d48dcf71cc184a1d1ae8fd3c8df9644345c
안혜연 <ahy020606@naver.com>
2024-11-21 16:15:00 +0900

    Feat: 상품 등록

--- noeyeyh/5주차/jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
+++ noeyeyh/5주차/jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.web;
 
+import jpabook.jpashop.Service.MemberService;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
