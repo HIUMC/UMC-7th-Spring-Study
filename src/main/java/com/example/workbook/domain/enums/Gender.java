a3400fda2f0e2c28d32a76ac0e26d3393e78204f
Jiyun <kjy5952@naver.com>
2024-11-27 17:51:26 +0900

    Feat(kjiyun):8주차 워크북 제출

--- src/main/java/com/example/workbook/domain/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.workbook.domain.enums;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- src/main/java/com/example/workbook/repository/StoreRepository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.workbook.repository.StoreRepository;
-
-//import com.example.workbook.domain.Store;
-//import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
-//}
